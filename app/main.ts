3c5d33973e611d162e69b3409e784eb08d60f398
ankur <best.ankur@gmail.com>
2016-05-14 01:31:19 +0200

    working example with rc1

--- app/main.ts
+++ app/main.ts
@@ -5,3 +5,8 @@ import {FORM_PROVIDERS, FormBuilder, Validators} from '@angular/common';
 import {TodoService} from './todo-service';
 
 bootstrap(AppDevice, [HTTP_PROVIDERS,FORM_PROVIDERS, TodoService]);
+
+// import {bootstrap}    from '@angular/platform-browser-dynamic';
+// import {AppComponent} from './app.component';
+//
+// bootstrap(AppComponent);
