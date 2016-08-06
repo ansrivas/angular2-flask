46b1cd1904d6e3af5802fbc84215bcb1502fe93d
Luke Farran <luke.farran@ubnt.com>
2016-08-06 06:45:08 -0500

    Removing unused dependencies from systemjs.config

--- systemjs.config.js
+++ systemjs.config.js
@@ -27,9 +27,9 @@
     '@angular/http',
     '@angular/platform-browser',
     '@angular/platform-browser-dynamic',
-    '@angular/router-deprecated',
-    '@angular/testing',
-    '@angular/upgrade',
+    //'@angular/router-deprecated',
+    '@angular/testing'/*,
+    '@angular/upgrade',*/
   ];
 
   // add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
