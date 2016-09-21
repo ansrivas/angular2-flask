af82500a3a4fc8e6cd214124aca91f1ecb8b712c
Ankur Srivastava <best.ankur@gmail.com>
2016-09-21 23:23:34 +0200

    another fix for webdriver issue

--- backend/flask_app/config.py
+++ backend/flask_app/config.py
@@ -78,16 +78,25 @@ class TestingConfig(BaseConfig):
     SECRET_KEY = '792842bc-c4df-4de1-9177-d5207bd9faa6'
 
 
+class LogFormatter(logging.Formatter):
+    datefmt = '%Y-%m-%d %H:%M:%S'
+
+    def format(self, record):
+        error_location = "%s.%s" % (record.name, record.funcName)
+        line_number = "%s" % (record.lineno)
+        location_line = error_location[:32] + ":" + line_number
+        s = "%.19s [%-8s] [%-36s] %s" % (self.formatTime(record, self.datefmt),
+                                         record.levelname,  location_line, record.getMessage())
+        return s
+
+
 def setup_logger():
-    """Setup the logger with predefined formatting of time and rollup."""
+    """Set up the global logging settings."""
     generated_files = 'logs'
     ALL_LOG_FILENAME = '{0}/all.log'.format(generated_files)
     ERROR_LOG_FILENAME = '{0}/error.log'.format(generated_files)
     if not os.path.exists(generated_files):
         os.makedirs(generated_files)
-
-    format = "[%(asctime)s] [%(name)-30s] [%(levelname)-8s] %(message)s"
-    datefmt = '%Y-%m-%d %H:%M:%S'
 
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
@@ -95,23 +104,23 @@ def setup_logger():
     # create console handler and set level to info
     handler = logging.StreamHandler()
     handler.setLevel(logging.INFO)
-    handler.setFormatter(logging.Formatter(format, datefmt=datefmt))
+    handler.setFormatter(LogFormatter())
     logger.addHandler(handler)
 
     # create error file handler and set level to error
     handler = logging.handlers.RotatingFileHandler(ERROR_LOG_FILENAME,
                                                    maxBytes=1000000,
-                                                   backupCount=1000)
+                                                   backupCount=100)
     handler.setLevel(logging.ERROR)
-    handler.setFormatter(logging.Formatter(format, datefmt=datefmt))
+    handler.setFormatter(LogFormatter())
     logger.addHandler(handler)
 
     # create debug file handler and set level to debug
     handler = logging.handlers.RotatingFileHandler(ALL_LOG_FILENAME,
                                                    maxBytes=1000000,
-                                                   backupCount=1000)
+                                                   backupCount=100)
     handler.setLevel(logging.DEBUG)
-    handler.setFormatter(logging.Formatter(format, datefmt=datefmt))
+    handler.setFormatter(LogFormatter())
     logger.addHandler(handler)
 
     print('Logging into directory {}\n'.format(generated_files))
